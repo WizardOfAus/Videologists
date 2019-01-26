1e4aec0ccee9f7755ccb0af4e8bfaebe34ccf282
marklajs <jjm21@hotmail.co.uk>
2019-01-26 14:03:16 +0000

    Jonnys Commit

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,16 +1,50 @@
-let changeColor = document.getElementById('changeColor');
+// let changeColor = document.getElementById('changeColor');
+//
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
+//
+//
+// changeColor.onclick = function(element) {
+//   let color = element.target.value;
+//   chrome.tabs.query({
+//     active: true,
+//     currentWindow: true
+//   }, function(tabs) {
+//     chrome.tabs.executeScript(
+//       tabs[0].id, {
+//         code: 'document.body.style.backgroundColor = "' + color + '";'
+//       });
+//   });
+// };
 
-  chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-  });
+window.addEventListener('load', function(evt) {
+  document.getElementById('form')
+            .addEventListener('submit', saveEntry);
+});
 
 
-  changeColor.onclick = function(element) {
-      let color = element.target.value;
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "' + color + '";'});
-      });
-    };
+function load() {
+
+
+  // let address = element.target.value;
+  // let startTime = element.target.startTime;
+  // let endTime = element.target.endTime;
+  //
+  // chrome.storage.local.get('data', function (result) {
+  //       channels = result.channels;
+  //       alert(result.channels);
+  //       $("#channels").val(channels);
+  //   });
+}
+
+function saveEntry(){
+  event.preventDefault();
+  let formData = document.getElementById('form');
+
+  console.log(formData);
+  // chrome.storage.local.set({http: value}, function() {
+  //         console.log('Value is set to ' + value);
+  //       });
+}
